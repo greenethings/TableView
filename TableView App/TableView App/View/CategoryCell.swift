7158d2f48dfb7401966821ffc24702190117ad75
Yashim Greene <ykgreene@Yashims-MacBook-Pro.local>
2017-08-20 16:04:31 -0500

    phase01 complete - setup tableView and prototype tableView cell

---
+++ TableView App/TableView App/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  TableView App
+//
+//  Created by Yashim Greene on 8/20/17.
+//  Copyright © 2017 Yashim Greene. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
